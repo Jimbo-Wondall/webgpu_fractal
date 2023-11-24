41703c46f95da94dbec620b705597eb7d8a2d265
Jimbo-Wondall <109582514+Jimbo-Wondall@users.noreply.github.com>
2023-11-25 03:00:34 +1000

    Implemented fractal shader and mouse controls

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,77 @@
-use std::iter;
-
+use bytemuck::{Pod, Zeroable};
+use bytemuck_derive::{Pod, Zeroable};
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+use wgpu::util::DeviceExt;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
 
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Pod, Zeroable)]
+pub struct FractalParameters {
+    scale: f32,
+    offset_x: f32,
+    offset_y: f32,
+    max_iterations: u32,
+}
+
+impl FractalParameters {
+    pub fn new() -> FractalParameters {
+        FractalParameters {
+            scale: 0.1,
+            offset_x: -1.3,
+            offset_y: 0.0,
+            max_iterations: 5000,
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Pod, Zeroable)]
+struct Resolution {
+    width: f32,
+    height: f32,
+    _padding1: f32,
+    _padding2: f32,
+}
 
 struct State {
     surface: wgpu::Surface,
     device: wgpu::Device,
     queue: wgpu::Queue,
-    config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
+    config: wgpu::SurfaceConfiguration,
+    render_pipeline: wgpu::RenderPipeline,
+    resolution_buffer: wgpu::Buffer,
+    fractal_params: FractalParameters,
+    param_buffer: wgpu::Buffer,
+    bind_group: wgpu::BindGroup,
     window: Window,
+    last_mouse_pos: winit::dpi::PhysicalPosition<f64>,
+    is_mouse_pressed: bool,
 }
 
 impl State {
     async fn new(window: Window) -> Self {
         let size = window.inner_size();
 
+        let resolution = Resolution {
+            width: size.width as f32,
+            height: size.height as f32,
+            _padding1: 0.0,
+            _padding2: 0.0,
+        };
+
+        let fractal_params = FractalParameters::new();
+
         let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
             backends: wgpu::Backends::all(),
             ..Default::default()
         });
+
         let surface = unsafe { instance.create_surface(&window) }.unwrap();
 
         let adapter = instance
@@ -54,12 +100,14 @@ impl State {
             .unwrap();
 
         let surface_caps = surface.get_capabilities(&adapter);
+
         let surface_format = surface_caps
             .formats
             .iter()
             .copied()
             .find(|f| f.is_srgb())
             .unwrap_or(surface_caps.formats[0]);
+
         let config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
             format: surface_format,
@@ -71,13 +119,117 @@ impl State {
         };
         surface.configure(&device, &config);
 
+        let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+
+        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+            ],
+            label: Some("bind_group_layout"),
+        });
+
+        let resolution_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Resolution Buffer"),
+            contents: bytemuck::cast_slice(&[resolution]),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let param_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Fractal Parameters Buffer"),
+            contents: bytemuck::cast_slice(&[fractal_params]),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: resolution_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: param_buffer.as_entire_binding(),
+                },
+            ],
+            label: Some("bind_group"),
+        });
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[&bind_group_layout],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
+
         Self {
             surface,
             device,
             queue,
-            config,
             size,
+            config,
+            render_pipeline,
+            resolution_buffer,
+            fractal_params,
+            param_buffer,
+            bind_group,
             window,
+            last_mouse_pos: winit::dpi::PhysicalPosition::new(0.0, 0.0),
+            is_mouse_pressed: false,
         }
     }
 
@@ -91,15 +243,60 @@ impl State {
             self.config.width = new_size.width;
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
+
+            let resolution = Resolution {
+                width: new_size.width as f32,
+                height: new_size.height as f32,
+                _padding1: 0.0,
+                _padding2: 0.0,
+            };
+
+            self.queue.write_buffer(
+                &self.resolution_buffer,
+                0,
+                bytemuck::cast_slice(&[resolution]),
+            );
         }
     }
 
-    #[allow(unused_variables)]
     fn input(&mut self, event: &WindowEvent) -> bool {
-        false
+        match event {
+            WindowEvent::MouseWheel { delta, .. } => match delta {
+                winit::event::MouseScrollDelta::LineDelta(_, y) => {
+                    let zoom_factor: f32 = 1.1;
+                    self.fractal_params.scale *= zoom_factor.powf(*y as f32);
+                    self.update_params();
+                }
+                _ => {}
+            },
+            WindowEvent::MouseInput { state, button, .. } if *button == MouseButton::Left => {
+                self.is_mouse_pressed = *state == ElementState::Pressed;
+            }
+            WindowEvent::CursorMoved { position, .. } => {
+                if self.is_mouse_pressed {
+                    let dx = position.x - self.last_mouse_pos.x;
+                    let dy = position.y - self.last_mouse_pos.y;
+
+                    self.fractal_params.offset_x -= dx as f32 * 0.01;
+                    self.fractal_params.offset_y -= dy as f32 * 0.01;
+                    self.update_params();
+                }
+                self.last_mouse_pos = *position;
+            }
+            _ => return false,
+        }
+        true
     }
 
     fn update(&mut self) {}
+
+    pub fn update_params(&mut self) {
+        self.queue.write_buffer(
+            &self.param_buffer,
+            0,
+            bytemuck::cast_slice(&[self.fractal_params]),
+        );
+    }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         let output = self.surface.get_current_texture()?;
@@ -112,7 +309,7 @@ impl State {
                 label: Some("Render Encoder"),
             });
         {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     view: &view,
@@ -124,11 +321,16 @@ impl State {
                             b: 0.3,
                             a: 1.0,
                         }),
-                        store: true,
+                        store: wgpu::StoreOp::Store,
                     },
                 })],
                 depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
             });
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_bind_group(0, &self.bind_group, &[]);
+            render_pass.draw(0..3, 0..1);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
@@ -171,51 +373,48 @@ pub async fn run() {
 
     let mut state = State::new(window).await;
 
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent {
-                ref event,
-                window_id,
-            } if window_id == state.window().id() => {
-                if !state.input(event) {
-                    // UPDATED!
-                    match event {
-                        WindowEvent::CloseRequested
-                        | WindowEvent::KeyboardInput {
-                            input:
-                                KeyboardInput {
-                                    state: ElementState::Pressed,
-                                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                                    ..
-                                },
-                            ..
-                        } => *control_flow = ControlFlow::Exit,
-                        WindowEvent::Resized(physical_size) => {
-                            state.resize(*physical_size);
-                        }
-                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                            state.resize(**new_inner_size);
-                        }
-                        _ => {}
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == state.window().id() => {
+            if !state.input(event) {
+                match event {
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    } => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
                     }
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        state.resize(**new_inner_size);
+                    }
+                    _ => {}
                 }
             }
-            Event::RedrawRequested(window_id) if window_id == state.window().id() => {
-                state.update();
-                match state.render() {
-                    Ok(_) => {}
-                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
-                        state.resize(state.size)
-                    }
-                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-
-                    Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
-                }
-            }
-            Event::RedrawEventsCleared => {
-                state.window().request_redraw();
-            }
-            _ => {}
         }
+        Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                    state.resize(state.size)
+                }
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+
+                Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+            }
+        }
+        Event::RedrawEventsCleared => {
+            state.window().request_redraw();
+        }
+        _ => {}
     });
 }
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,38 +1,59 @@
-[[block]]
-struct Constants {
-    width: u32;
-    height: u32;
-    scale: f32;
-    offsetX: f32;
-    offsetY: f32;
-    maxIterations: u32;
+struct VertexOutput {
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) vert_pos: vec3<f32>,
 };
 
-[[group(0), binding(0)]]
-var<uniform> constants: Constants;
-
-[[stage(vertex)]]
-fn vertex_main(
-    [[builtin(vertex_index)]] vertexIndex: u32
-) -> [[builtin(position)]] vec4<f32> {
-    var x: f32 = f32(iRem(vertexIndex, constants.width));
-    var y: f32 = f32(iDiv(vertexIndex, constants.width));
-    x = (x / f32(constants.width - 1u)) * 2.0 - 1.0;
-    y = (y / f32(constants.height - 1u)) * 2.0 - 1.0;
-    return vec4<f32>(x, y, 0.0, 1.0);
+@vertex
+fn vs_main(@builtin(vertex_index) index: u32) -> VertexOutput {
+    var out: VertexOutput;
+    let x = f32(1 - i32(index)) * 3.0;
+    let y = f32(i32(index & 1u) * 2 - 1) * 3.0;
+    out.clip_position = vec4<f32>(x, y, 0.0, 1.0);
+    out.vert_pos = out.clip_position.xyz;
+    return out;
 }
 
-[[stage(fragment)]]
-fn fragment_main(
-    [[builtin(position)]] position: vec4<f32>
-) -> [[location(0)]] vec4<f32> {
-    var c_re: f32 = position.x * constants.scale + constants.offsetX;
-    var c_im: f32 = position.y * constants.scale + constants.offsetY;
+struct Resolution {
+    height: f32,
+    width: f32,
+    _padding1: f32,
+    _padding2: f32,
+};
+
+@group(0) @binding(0)
+var<uniform> iResolution: Resolution;
+
+struct FractalParameters {
+    scale: f32,
+    offset_x: f32,
+    offset_y: f32,
+    max_iterations: u32,
+};
+
+@group(0) @binding(1)
+var<uniform> params: FractalParameters;
+
+@fragment
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    // Pixel coordinates
+    let frag = in.clip_position.xy; 
+    // Current screen resolution as vec2
+    let res = vec2<f32>(iResolution.height, iResolution.width); 
+    // Current coordinates mapped to a system where 0,0 is center of screen
+    // and y=1 is bottom edge and x is scaled to screen width
+    let uv = (frag.xy * 2.0 - res.xy) / res.y; 
+
+    //let scale: f32 = 0.1;
+    //let offset_x: f32 = -1.3;
+    //let offset_y: f32 = 0.0;
+    //let max_iterations: u32 = 5000u;
+    var c_re: f32 = uv.x * params.scale + params.offset_x;
+    var c_im: f32 = uv.y * params.scale + params.offset_y;
     var z_re: f32 = 0.0;
     var z_im: f32 = 0.0;
     var i: u32 = 0u;
 
-    for (; i < constants.maxIterations; i = i + 1u) {
+    for (; i < params.max_iterations; i = i + 1u) {
         var z_re2: f32 = z_re * z_re;
         var z_im2: f32 = z_im * z_im;
         if (z_re2 + z_im2 > 4.0) {
@@ -44,6 +65,6 @@ fn fragment_main(
         z_im = new_im;
     }
 
-    var color: f32 = f32(i) / f32(constants.maxIterations);
+    var color: f32 = f32(i) / f32(params.max_iterations);
     return vec4<f32>(color, color, color, 1.0);
 }
